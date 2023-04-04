07876e15b18bb7ba9d6aa5429054192198218ce7
Yevhen Vasylevytskyi <e.vasylevytskyi@gmail.com>
2023-04-04 06:13:33 +0300

    add Feedback

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+// import PropTypes from 'prop-types';
+// import s from './Feedback.module.css';
+
+function Feedback() {
+  return (
+    <p>
+      Feedback
+    </p>
+  );
+}
+// Feedback.propTypes = {
+//   text: PropTypes.string.isRequired,  
+// };
+
+export default Feedback;
